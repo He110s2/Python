f7ba1f6f78740feef4c77a6a2ed3999b6d1b1ad7
Helloiza <helloiza.silva@escola.pr.gov.br>
2025-05-06 12:28:54 -0300

    commit inicial

---
+++ pythonProject1/exemplo 2 - nomes variavel.py
@@ -0,0 +1,10 @@
+#variáveis não podem começar com números e nem com letra maiúscula
+#variáveis não podem ter caracteres especias, tirando o __
+#76 trombones = 'big parentes'
+
+n = 10
+m = 25
+
+total = n + m
+
+print(total)
