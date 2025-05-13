91a770cf1295abbd06d8585616dcf4923e188595
Hello <helloiza.silva@escola.pr.gov.br>
2025-05-13 11:33:06 -0300

    capitulo 1
    exemplo operadores aritméticos

---
+++ pythonProject3/capitulo 1/exemplo1operadoresaritmeticos.py
@@ -0,0 +1,14 @@
+#operador de soma
+soma = 1 + 2
+
+#operador de subtração
+subtração = 2 - 1
+
+#operador de multiplicação
+multiplicação = 2 * 1
+
+#operador de divisão
+divisao = 2 / 1
+
+#operador de exponenciacao (potencia)
+exponenciacao = 2 ** 1 
