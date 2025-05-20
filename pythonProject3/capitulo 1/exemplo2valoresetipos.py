06d319bab4659c76574a257a646b22b75a7235d9
Hello <helloiza.silva@escola.pr.gov.br>
2025-05-20 11:39:10 -0300

    capitulo 1
    Valores e tipos

---
+++ pythonProject3/capitulo 1/exemplo2valoresetipos.py
@@ -0,0 +1,14 @@
+#tipo inteiro 1,2,3...
+inteiro = 2
+
+#tipo real ou ponto flutuante FLOAT 1.3, 8.6.... -3.646...
+float = 1.3
+print(type(float))
+
+#caracteres
+char1 = 'a'
+char2 = '2'
+print(type(char1))
+#string conjunto de caracteres
+stringNome = 'Helloiza'
+print(type(stringNome))
